b36d55df2acda19d1de5e8c4143eb4ee4704290f
sohrab11761 <sohrabk124@gmail.com>
2021-10-04 16:20:23 +0500

    sidebar + home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Navbar from '../src/components/navbar/Navbar';
 import Routes from '../src/components/routes/Routes';
 import { BrowserRouter } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
         <Navbar />
-         
+         <Home/>
         <Routes />
       </BrowserRouter>
     </div>
