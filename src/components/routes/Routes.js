de731983b2f2b028471a9ef68c3037ff6bb0c7cf
Sameerahmadkhan <samerrkaan9@gmail.com>
2021-10-04 03:28:59 -0700

    Chatroom done

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Switch, Route } from "react-router-dom";
+import Chatroom from '../Chatroom/Chatroom';
 import Profile from "../profile/Profile";
 export default function Routes() {
     return (
         <div>
             <Switch>
                 <Route path="/profile" component={Profile}/>
-
+                <Route path="/chatroom" component={Chatroom}/>
             </Switch>
         </div>
     )
