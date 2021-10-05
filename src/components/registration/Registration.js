a7007cf3a2ec8fa79e2ad4b1a522e8d4073088d7
sohrab11761 <sohrabk124@gmail.com>
2021-10-05 16:53:36 +0500

    changes in auth

--- src/components/registration/Registration.js
+++ src/components/registration/Registration.js
@@ -42,86 +42,92 @@ export default function Registration() {
             </h1>
           </div>
           <div>
-          <div className="Data d-flex justify-content-center  align-items-center py-4">
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label float-start">
-                <b>UserName</b>
-              </label>
-              <input
-                type="text"
-                class="form-control  d-flex  justify-content-center align-items-center "
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                onChange={(e) => {
-                  setUserName(e.target.value);
-                }}
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label float-start">
-                <b>Email</b>
-              </label>
-              <input
-                type="email"
-                class="form-control  d-flex  justify-content-center align-items-center "
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label float-start">
-                <b> Password </b>
-              </label>
-              <input
-                type="password"
-                class="form-control d-flex justify-content-center align-items-center"
-                id="exampleInputPassword1"
-                onChange={(e) => {
-                  setUserPassword(e.target.value);
-                }}
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label float-start">
-                <b>Re-type Password </b>
-              </label>
-              <input
-                type="password"
-                class="form-control d-flex justify-content-center align-items-center"
-                id="exampleInputPassword1"
-                onChange={(e) => {
-                  setRetypePassword(e.target.value);
-                }}
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleAddress" class="form-label float-start">
-                <b> Company Name</b>
-              </label>
-              <input
-                type="text"
-                class="form-control d-flex justify-content-center align-items-center"
-                id="exampleAddress"
-                aria-describedby="emailHelp"
-                onChange={(e) => {
-                  setcompanyName(e.target.value);
-                }}
-              />
-            </div>
+            <div className="Data d-flex justify-content-center  align-items-center py-4">
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label float-start">
+                  <b>UserName</b>
+                </label>
+                <input
+                  type="text"
+                  class="form-control  d-flex  justify-content-center align-items-center "
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  onChange={(e) => {
+                    setUserName(e.target.value);
+                  }}
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label float-start">
+                  <b>Email</b>
+                </label>
+                <input
+                  type="email"
+                  class="form-control  d-flex  justify-content-center align-items-center "
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </div>
+              <div class="mb-3">
+                <label
+                  for="exampleInputPassword1"
+                  class="form-label float-start"
+                >
+                  <b> Password </b>
+                </label>
+                <input
+                  type="password"
+                  class="form-control d-flex justify-content-center align-items-center"
+                  id="exampleInputPassword1"
+                  onChange={(e) => {
+                    setUserPassword(e.target.value);
+                  }}
+                />
+              </div>
+              <div class="mb-3">
+                <label
+                  for="exampleInputPassword1"
+                  class="form-label float-start"
+                >
+                  <b>Re-type Password </b>
+                </label>
+                <input
+                  type="password"
+                  class="form-control d-flex justify-content-center align-items-center"
+                  id="exampleInputPassword1"
+                  onChange={(e) => {
+                    setRetypePassword(e.target.value);
+                  }}
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleAddress" class="form-label float-start">
+                  <b> Company Name</b>
+                </label>
+                <input
+                  type="text"
+                  class="form-control d-flex justify-content-center align-items-center"
+                  id="exampleAddress"
+                  aria-describedby="emailHelp"
+                  onChange={(e) => {
+                    setcompanyName(e.target.value);
+                  }}
+                />
+              </div>
 
-            <button
-              type="submit"
-              class=" Button form-control btn btn-danger mt-3 mb-3"
-              onClick={() => {
-                handleSendMessage();
-              }}
-            >
-              Register
-            </button>
-          </div>
+              <button
+                type="submit"
+                class=" Button form-control btn btn-danger mt-3 mb-3"
+                onClick={() => {
+                  handleSendMessage();
+                }}
+              >
+                Register
+              </button>
+            </div>
           </div>
         </div>
       </div>
