c0b57ff300d59a10bbb83669671799e4b574ab15
@ihteshamu11 <ihteshamu11@gmail.com>
2021-10-27 03:56:25 -0700

    add pic

--- src/components/Chatroom/Chatroom.js
+++ src/components/Chatroom/Chatroom.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Chatroom.css';
-import logo from '../../assets/logo.png'
+import sveaTaxi from '../../assets/images/SveaTaxi.png'
+import microsoftteams from '../../assets/images/MicrosoftTeams.png'
+import sverigetaxi from '../../assets/images/Sverigetaxi.jpg'
+import T1212 from '../../assets/images/T1212.jpg'
+import TKurir from '../../assets/images/TKurir.jpg'
+import TSkane from '../../assets/images/TSkane.jpg'
 import { useHistory } from 'react-router';
 export default function Chatroom() {
     let history = useHistory();
@@ -14,28 +19,33 @@ export default function Chatroom() {
                     </h2>
                 </div>
                 <div className="hello mt-5 col-sm-10 offset-1" onClick={() => history.push(`/company/${2}`)}>
-                    <div className="img ps-2 pt-2"><img src={logo} className="image" /></div>
-                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Taxi 97</h5>
+                    <div className="img ps-2 pt-2 mt-4"><img src={sveaTaxi} className="image" /></div>
+                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Svea Taxi</h5>
                         <p className="taxi ">Description of Taxi 97</p></div>
                 </div>
                 <div className="hello mt-5 col-sm-10 offset-1">
-                    <div className="img ps-2 pt-2"><img src={logo} className="image" /></div>
-                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Free Akare</h5>
+                    <div className="img ps-2 pt-2 mt-4"><img src={microsoftteams} className="image" /></div>
+                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Microsoft Teams</h5>
                         <p className="taxi ">Description of  Free Akare</p></div>
                 </div>
                 <div className="hello mt-5 col-sm-10 offset-1">
-                    <div className="img ps-2 pt-2"><img src={logo} className="image" /></div>
-                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Taxi 23</h5>
+                    <div className="img ps-2 pt-2 mt-4"><img src={sverigetaxi} className="image" /></div>
+                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Sverige Taxi</h5>
                         <p className="taxi ">Description of chatroom Taxi 23</p></div>
                 </div>
                 <div className="hello mt-5 col-sm-10 offset-1">
-                    <div className="img ps-2 pt-2"><img src={logo} className="image" /></div>
-                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Taxi 59</h5>
+                    <div className="img ps-2 pt-2 mt-4"><img src={T1212} className="image" /></div>
+                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Taxi 1212</h5>
                         <p className="taxi ">Description of chatroom Taxi 59</p></div>
                 </div>
                 <div className="hello mt-5 col-sm-10 offset-1">
-                    <div className="img ps-2 pt-2"><img src={logo} className="image" /></div>
-                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Taxi 1212</h5>
+                    <div className="img ps-2 pt-2 mt-4"><img src={TKurir} className="image" /></div>
+                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Taxi kurir</h5>
+                        <p className="taxi ">Description of chatroom Taxi 1212</p></div>
+                </div>
+                <div className="hello mt-5 col-sm-10 offset-1">
+                    <div className="img ps-2 pt-2 mt-4"><img src={TSkane} className="image" /></div>
+                    <div className="col-8 ps-4 pt-3"> <h5 className="taxi ">Taxi kurir</h5>
                         <p className="taxi ">Description of chatroom Taxi 1212</p></div>
                 </div>
 
