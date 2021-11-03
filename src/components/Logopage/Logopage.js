4a446dc6f58ae5fc72e83a46d042d7c83e7fbc90
@ihteshamu11 <ihteshamu11@gmail.com>
2021-11-02 22:46:00 -0700

    color and font-color change 2 page add

--- src/components/Logopage/Logopage.js
+++ src/components/Logopage/Logopage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Logopage.css'
+import trafikinfo from "../../components/Logopage/trafikinfo.png"
 function Logopage() {
     return (
         <div className="Container-fluid-logopage">
         <div className="Container-fluid-images">
-            
+            <h2><b>Welcome to Trafik info</b></h2>
+        <br/>
+        <br/>
+        <br/>
+            <img className="imgsss" src={trafikinfo} style={{textAlign:"center",alignItems:"center", height: "50%", width: "50%", }} />
         </div>
         </div>
     )
