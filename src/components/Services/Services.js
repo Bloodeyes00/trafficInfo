f08e75220e82a6b819fa7ceb712865288bf5452a
@ihteshamu11 <ihteshamu11@gmail.com>
2021-11-10 17:03:59 +0500

    sdsd

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -4,6 +4,9 @@ import React from 'react'
 import '../Services/Services.css'
 import { Card } from 'react-bootstrap';
 import car3 from '../Services/car3.jpg'
+import car4 from '../Services/car4.jpg'
+import car5 from '../Services/car5.jpg'
+import { Carousel } from 'react-bootstrap'
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 
 const Services = () => {
@@ -14,10 +17,57 @@ const Services = () => {
     { image: "", title: "Motor",  price: 'RS:2000000', model: 'Model:2002', registeration: 'Reg/No:23232' },
     { image: "", title: "Motor",  price: 'RS:2000000', model: 'Model:2002', registeration: 'Reg/No:23232' },];
     const renderCard = (card, index) => {
+        const responsive = {
+            superLargeDesktop: {
+              // the naming can be any, depends on you.
+              breakpoint: { max: 4000, min: 3000 },
+              items: 5
+            },
+            desktop: {
+              breakpoint: { max: 3000, min: 1024 },
+              items: 3
+            },
+            tablet: {
+              breakpoint: { max: 1024, min: 464 },
+              items: 2
+            },
+            mobisle: {
+              breakpoint: { max: 464, min: 0 },
+              items: 1
+            }
+          };
         return (
         <div className="container-card">
         <Card className="cardsss" style={{ width: '18rem' }} key={index} style={{ display: 'flex', marginBottom: '5px' }}>
+       
+     <div className="carousel-container">
+        <Carousel swipeable={false}
+          draggable={false}
+          // showDots={true}
+          responsive={responsive}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={2000}
+          // keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={700}>
+          <Carousel variant="dark">
+
+            <Carousel.Item>
             <center><Card.Img variant="top" src={car3} style={{ width: '60%', height: '150px', borderRadius: '2px', display: 'flex', marginTop:"10px" }} /></center>
+            </Carousel.Item>
+            <Carousel.Item>
+            <center><Card.Img variant="top" src={car4} style={{ width: '60%', height: '150px', borderRadius: '2px', display: 'flex', marginTop:"10px" }} /></center>
+            </Carousel.Item>
+            <Carousel.Item>
+            <center><Card.Img variant="top" src={car5} style={{ width: '60%', height: '150px', borderRadius: '2px', display: 'flex', marginTop:"10px" }} /></center>
+            </Carousel.Item>
+          </Carousel>
+        </Carousel>
+
+      </div>
+            {/* <center><Card.Img variant="top" src={car3} style={{ width: '60%', height: '150px', borderRadius: '2px', display: 'flex', marginTop:"10px" }} /></center> */}
            
             <Card.Body className="body">
                 <Card.Title>{card.title}</Card.Title>
